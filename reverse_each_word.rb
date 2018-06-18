d1238c9a113b219c567ebc576e613d341a252e42
Michael Morris <mdam2345@gmail.com>
2018-06-18 19:50:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
     nuarray = []
-    array.map! do |element|
+    array.map! do |sentence|
       element.reverse
-    end 
+    end
   array.join(" ")
-  end 
+  end
 end
