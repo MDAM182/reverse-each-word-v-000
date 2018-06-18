a2cec345cfc3a2907fb503f8689270f693d0241d
Michael Morris <mdam2345@gmail.com>
2018-06-18 20:04:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-def reverse_words(str)
-  words = str.split(' ')
-  reverse_str = []
-
-  words.length.times do |i|
-    reverse_str[i] = words[i].reverse
-  end
-
-
-  return reverse_str.join(" ")
+def reverse_each_word(sentence)
+  
 end
